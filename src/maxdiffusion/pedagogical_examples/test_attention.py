7010c09787158e90b71284a635992967805dc611
Juan Acevedo <jfacevedo@google.com>
2024-03-06 00:39:11 +0000

    only eval shape during pytorch to jax weight conversion. Fix minor errors.

--- src/maxdiffusion/pedagogical_examples/test_attention.py
+++ src/maxdiffusion/pedagogical_examples/test_attention.py
@@ -85,49 +85,20 @@ def run_time_comparison():
   flash_norm = jnp.linalg.norm(flash_attention_out)
   print(f"{flash_norm=}")
 
-  # # jax.profiler.start_trace("gs://maxdiffusion-experiments-multipod/mattdavidow-flash-trace")
-  # ### Flash timing
-  # query_for_flash = reshape_data_for_flash(query_states, heads)
-  # key_for_flash = reshape_data_for_flash(key_states, heads)
-  # value_for_flash = reshape_data_for_flash(value_states, heads)
-  # jit_flash = jax.jit(tpu_flash_attention_block)
-  # jit_flash(query_for_flash, key_for_flash * scale, value_for_flash).block_until_ready()
 
-  # start_time = time.perf_counter()
-  # for _ in range(n_trials):
-  #   jit_flash(query_for_flash, key_for_flash * scale, value_for_flash).block_until_ready()
-
-  # end_time = time.perf_counter()
-
-  # total_time = end_time - start_time
-  # average_time = total_time / n_trials
-  # print("\n\n Flash attention Average time:", average_time, "seconds per run")
-
-  # ##### regular timing
-  # if split_head_dim:
-  #   regular_func = regular_attention_split
-  # else:
-  #   regular_func = regular_attention_unsplit
-  # jit_regular = jax.jit(regular_func, static_argnums=(3,))
-  # jit_regular(query_states, key_states, value_states, heads).block_until_ready()
-  # start_time = time.perf_counter()
-  # for _ in range(n_trials):
-  #   jit_regular(query_states, key_states, value_states, heads).block_until_ready()
-
-  # end_time = time.perf_counter()
-
-  # total_time = end_time - start_time
-  # average_time = total_time / n_trials
-  # print("\n\n Regular attention Average time:", average_time, "seconds per run")
-  # jax.profiler.stop_trace()
-
-
-global batch; batch = 8
-global heads; heads = 10
-global length; length = 4096
-global head_depth; head_depth = 128
-global n_trials; n_trials = 10
-global split_head_dim; split_head_dim = True
-global scale; scale = 1.0 / jnp.sqrt(head_depth)
+global batch
+batch = 8
+global heads
+heads = 10
+global length
+length = 4096
+global head_depth
+head_depth = 64
+global n_trials
+n_trials = 10
+global split_head_dim
+split_head_dim = True
+global scale
+scale = 1.0 / jnp.sqrt(head_depth)
 
 run_time_comparison()
