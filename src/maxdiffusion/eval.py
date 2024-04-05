fix fid score calculation. Evaluate with the full coco dataset.

src/maxdiffusion/eval.py
@@ -43,21 +43,10 @@ def load_stats(file_path):
     return sigma, mu
 
 def eval(config):
-    captions_df = load_captions(config.caption_coco_file)
     batch_size = config.per_device_batch_size * jax.device_count()
 
     #inference happenning here: 
-
-    # while(not captions_df.empty):
-    #     if (captions_df.size > batch_size):
-    #         captions_df['caption'][:batch_size].to_csv('prompts.txt', header=False, index=False)
-    #         captions_df['id'][:batch_size].to_csv('ids.txt', header=False,index=False)
-    #         captions_df = captions_df.iloc[batch_size:]
-    #     else:
-    #         captions_df['caption'].to_csv('prompts.txt', header=False, index=False)
-    #         captions_df['id'].to_csv('ids.txt', header=False,index=False)
-    #         captions_df = pd.DataFrame()
-    #     generate.run(config)
+    generate.run(config)
 
     rng = jax.random.PRNGKey(0)
     
@@ -65,22 +54,15 @@ def eval(config):
     params = model.init(rng, jnp.ones((1, 256, 256, 3)))
 
     apply_fn = jax.jit(functools.partial(model.apply, train=False))
-    mu, sigma = fid_score.compute_statistics(config.images_directory, params, apply_fn, batch_size)
+    mu, sigma = fid_score.compute_statistics_with_mmap(config.images_directory, "/tmp/temp.dat", params, apply_fn, batch_size, (config.resolution, config.resolution))
     os.makedirs(config.stat_output_directory, exist_ok=True)
     np.savez(os.path.join(config.stat_output_directory, 'stats'), mu=mu, sigma=sigma)
 
-    #     with open(config.image_ids, "r") as ids_file:
-    #         image_ids = ids_file.readlines()
-    #         for image_id in image_ids:
-    #             pathlib.Path(f"image_{image_id}.png").unlink()
-
-    mu1, sigma1 = fid_score.compute_statistics(config.stat_output_file, params, apply_fn, batch_size)
-    mu2, sigma2 = fid_score.compute_statistics(config.stat_coco_file, params, apply_fn, batch_size)
+    mu1, sigma1 = fid_score.compute_statistics(config.stat_output_file, params, apply_fn, batch_size,)
+    mu2, sigma2 = fid_score.compute_statistics(config.stat_coco_file, params, apply_fn, batch_size,)
 
     fid = fid_score.compute_frechet_distance(mu1, mu2, sigma1, sigma2, eps=1e-6)
-    print("fid is : " + str(fidscore))
-
-
+    print("fid is : " + str(fid))
 
 def main(argv: Sequence[str]) -> None:
     pyconfig.initialize(argv)

src/maxdiffusion/generate.py
@@ -18,8 +18,9 @@ import functools
 import os
 import time
 from typing import Sequence
-
+import csv
 import numpy as np
+from tqdm import tqdm
 
 import jax
 import jax.numpy as jnp
@@ -41,6 +42,8 @@ from jax.experimental.compilation_cache import compilation_cache as cc
 from jax.sharding import Mesh, PositionalSharding
 from maxdiffusion.image_processor import VaeImageProcessor
 from PIL import Image
+
+from multiprocessing import Process
 
 cc.initialize_cache(os.path.expanduser("~/jax_cache"))
 
@@ -76,15 +79,8 @@ def tokenize(prompt, tokenizer):
         return_tensors="np"
     ).input_ids
 
-def get_unet_inputs(rng, config, batch_size, pipeline, params):
+def get_unet_inputs(rng, config, batch_size, pipeline, params, prompt_ids, negative_prompt_ids):
     vae_scale_factor = 2 ** (len(pipeline.vae.config.block_out_channels) - 1)
-    prompts = []
-    with open(config.prompts, "r") as captions_file:
-        prompts = captions_file.readlines()
-    prompt_ids = prompts[:batch_size]
-    prompt_ids = tokenize(prompt_ids, pipeline.tokenizer)
-    negative_prompt_ids = [config.negative_prompt] * batch_size
-    negative_prompt_ids = tokenize(negative_prompt_ids, pipeline.tokenizer)
     guidance_scale = config.guidance_scale
     num_inference_steps = config.num_inference_steps
 
@@ -121,6 +117,12 @@ def vae_decode(latents, state, pipeline):
     image = (image / 2 + 0.5).clip(0, 1).transpose(0, 2, 3, 1)
     return image
 
+def save_process(images, config, img_ids):
+    images = VaeImageProcessor.numpy_to_pil(images)
+    image_directory = config.images_directory
+    for i, image in enumerate(images):
+        image.save(f"{image_directory}image_{img_ids[i]}.png")
+
 def run(config):
     rng = jax.random.PRNGKey(config.seed)
     # Setup Mesh
@@ -128,7 +130,7 @@ def run(config):
     mesh = Mesh(devices_array, config.mesh_axes)
 
     batch_size = jax.device_count() * config.per_device_batch_size
-
+    assert 30_000 % batch_size == 0, f"Coco dataset must be evenly divisible by batch size : {batch_size}"
     weight_dtype = get_dtype(config)
     flash_block_sizes = get_flash_block_sizes(config)
     pipeline, params = FlaxStableDiffusionPipeline.from_pretrained(
@@ -157,49 +159,60 @@ def run(config):
     del params["vae"]
     del params["unet"]
 
-    def run_inference(unet_state, vae_state, params, rng, config, batch_size, pipeline):
-
+    def run_inference(unet_state, vae_state, params, prompt_ids, negative_prompt_ids, rng, config, batch_size, pipeline):                
         (latents,
         context,
         guidance_scale,
-        scheduler_state) = get_unet_inputs(rng, config, batch_size, pipeline, params)
-
-        loop_body_p = functools.partial(loop_body, model=pipeline.unet,
-                                        pipeline=pipeline,
-                                        prompt_embeds=context,
-                                        guidance_scale=guidance_scale)
-
-        vae_decode_p = functools.partial(vae_decode, pipeline=pipeline)
-
+        scheduler_state) = get_unet_inputs(rng, config, batch_size, pipeline, params, prompt_ids, negative_prompt_ids)
         with mesh, nn_partitioning.axis_rules(config.logical_axis_rules):
+            loop_body_p = functools.partial(loop_body, model=pipeline.unet,
+                                            pipeline=pipeline,
+                                            prompt_embeds=context,
+                                            guidance_scale=guidance_scale)
+
+            vae_decode_p = functools.partial(vae_decode, pipeline=pipeline)
             latents, _, _ = jax.lax.fori_loop(0, config.num_inference_steps,
                                             loop_body_p, (latents, scheduler_state, unet_state))
-            image = vae_decode_p(latents, vae_state)
-            return image
+            images = vae_decode_p(latents, vae_state)
+            return images
+                
 
     p_run_inference = jax.jit(
         functools.partial(run_inference, rng=rng, config=config, batch_size=batch_size, pipeline=pipeline),
-        in_shardings=(unet_state_mesh_shardings, vae_state_mesh_shardings, None),
-        out_shardings=None
+        in_shardings=(unet_state_mesh_shardings, vae_state_mesh_shardings, None, None, None),
+        out_shardings=None,
     )
 
-    s = time.time()
-    p_run_inference(unet_state, vae_state, params).block_until_ready()
-    print("compile time: ", (time.time() - s))
+    # s = time.time()
+    prompts = []
+    threads = []
+    clear_threads_count_at = 100
+    k = 0
+    with open(config.caption_coco_file, "r") as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        rows = list(rd)[1:]
+        negative_prompt_ids = tokenize([""] * batch_size, pipeline.tokenizer)
+        for i in tqdm(range(0, len(rows),  batch_size)):
+            img_ids = [row[0] for row in rows[i:i+batch_size]]
+            ids = [row[1] for row in rows[i:i+batch_size]]
+            prompts = [row[2] for row in rows[i:i+batch_size]]
+            prompt_ids = tokenize(prompts, pipeline.tokenizer)
+    
+            images = p_run_inference(unet_state, vae_state, params, prompt_ids, negative_prompt_ids)
+            images = jax.experimental.multihost_utils.process_allgather(images)
+            numpy_images = np.array(images)
+            p = Process(target=save_process, args=(numpy_images, config, img_ids))
+            p.start()
+            threads.append(p)
+            k+=1
+            if k >= clear_threads_count_at:
+                for thread in threads:
+                    thread.join()
+                    threads = []
+                    k = 0
 
-    s = time.time()
-    images = p_run_inference(unet_state, vae_state, params).block_until_ready()
-    print("inference time: ",(time.time() - s))
-    numpy_images = np.array(images)
-    images = VaeImageProcessor.numpy_to_pil(numpy_images)
-    with open(config.image_ids, "r") as ids_file:
-        image_ids = ids_file.readlines()
-    image_directory = config.images_directory
-    for i, image in enumerate(images):
-        image.save(f"{image_directory}image_{image_ids[i]}.png")
-
-    return images
-
+        for thread in threads:
+            thread.join()
 def main(argv: Sequence[str]) -> None:
     pyconfig.initialize(argv)
     run(pyconfig.config)

src/maxdiffusion/models/train.py
@@ -38,20 +38,20 @@ from maxdiffusion import (
     eval
 )
 
-from transformers import FlaxCLIPTextModel
-
-from maxdiffusion.pipelines.stable_diffusion import FlaxStableDiffusionSafetyChecker
 from flax.linen import partitioning as nn_partitioning
 from jax.experimental.compilation_cache import compilation_cache as cc
 from jax.sharding import Mesh
 from jax.sharding import PartitionSpec as P, PositionalSharding
 from tensorboardX import SummaryWriter
-from transformers import CLIPImageProcessor, set_seed
+from transformers import set_seed
 
 from maxdiffusion.input_pipeline.input_pipeline_interface import (
   make_pokemon_train_iterator,
   make_laion400m_train_iterator
 )
+
+TOTAL_TRAIN_SAMPLES = 6513144
+TOTAL_EVAL_SAMPLES = 30000
 
 def calculate_training_tflops(pipeline, unet_params, config):
     """Calculate per device training tflops (back and fwd pass)."""
@@ -177,6 +177,10 @@ def train(config):
     data_sharding = jax.sharding.NamedSharding(mesh,P(*config.data_sharding))
 
     total_train_batch_size = config.per_device_batch_size * jax.device_count()
+    if config.checkpoint_every % total_train_batch_size != 0:
+        max_logging.log(f"Total train steps of {TOTAL_TRAIN_SAMPLES} is not evenly divisible by"
+                        f" global batch size of {total_train_batch_size}. Checkpointing might not"
+                        " work correctly.")
 
     weight_dtype = max_utils.get_dtype(config)
     flash_block_sizes = max_utils.get_flash_block_sizes(config)
@@ -367,6 +371,7 @@ def train(config):
     mllog_utils.train_init_stop()
     mllog_utils.train_run_start()
     mllog_utils.train_step_start(start_step)
+    # for checkpointing
     for step in np.arange(start_step, config.max_train_steps):
         example_batch = load_next_batch(data_iterator, example_batch, config)
         with mesh, nn_partitioning.axis_rules(config.logical_axis_rules):
@@ -374,17 +379,20 @@ def train(config):
                                                                 vae_state,
                                                                 example_batch,
                                                                 train_rngs)
-        
         new_time = datetime.datetime.now()
+
         record_scalar_metrics(train_metric, new_time - last_step_completion, per_device_tflops, learning_rate_scheduler(step))
         write_metrics(writer, train_metric, step, config)
         last_step_completion = new_time
 
-        if config.metrics_file:
+        if config.record_metrics and config.metrics_file:
             max_utils.write_metrics_locally(train_metric, step, config, local_metrics_file)
 
-        if config.gcs_metrics and jax.process_index() == 0:
+        if config.record_metrics and config.gcs_metrics and jax.process_index() == 0:
             running_gcs_metrics = max_utils.write_metrics_for_gcs(train_metric, step, config, running_gcs_metrics)
+
+        if step != 0 and (total_train_batch_size * step) % config.checkpoint_every == 0:
+           max_utils.save_checkpoint(pipeline, params, unet_state, noise_scheduler, config, config.output_dir+f"/{str(step * total_train_batch_size)}/")
 
         # Start profiling at end of first step to avoid compilation.
         # Move before for loop to include.
@@ -394,46 +402,11 @@ def train(config):
             max_utils.deactivate_profiler(config)
 
         mllog_utils.maybe_train_step_log(config, start_step, step, train_metric)
-        if (step%100 == 0): # and step != 0):
-            eval.eval(config)
+        # if (step%100 == 0): # and step != 0):
+        #     eval.eval(config)
 
     # Create the pipeline using using the trained modules and save it.
-    if jax.process_index() == 0:
-        safety_checker = FlaxStableDiffusionSafetyChecker.from_pretrained(
-            "CompVis/stable-diffusion-safety-checker", from_pt=True
-        )
-        # Restore vae and text encoder if we cached latents and encoder outputs.
-        if config.cache_latents_text_encoder_outputs:
-            text_encoder = FlaxCLIPTextModel.from_pretrained(
-                config.pretrained_model_name_or_path, revision=config.revision, subfolder="text_encoder", dtype=weight_dtype, from_pt=config.from_pt
-            )
-            vae, vae_params = FlaxAutoencoderKL.from_pretrained(
-                config.pretrained_model_name_or_path, revision=config.revision, subfolder="vae", dtype=weight_dtype, from_pt=config.from_pt
-            )
-            pipeline.vae = vae
-            pipeline.text_encoder = text_encoder
-            params["text_encoder"] = text_encoder.params
-            params["vae"] = vae_params
-
-        pipeline = FlaxStableDiffusionPipeline(
-            text_encoder=pipeline.text_encoder,
-            vae=pipeline.vae,
-            unet=pipeline.unet,
-            tokenizer=pipeline.tokenizer,
-            scheduler=noise_scheduler,
-            safety_checker=safety_checker,
-            feature_extractor=CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32"),
-        )
-
-        pipeline.save_pretrained(
-            config.output_dir,
-            params={
-                "text_encoder": get_params_to_save(params["text_encoder"]),
-                "vae": get_params_to_save(params["vae"]),
-                "unet": get_params_to_save(unet_state.params),
-                "safety_checker": safety_checker.params,
-            },
-        )
+    max_utils.save_checkpoint(pipeline, params, unet_state, noise_scheduler, config, config.output_dir+f"/{str(step * total_train_batch_size)}/")
     writer.close()
 
 def main(argv: Sequence[str]) -> None: