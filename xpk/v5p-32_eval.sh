def56fe0c1d814c854634cdb582c3e3fac07429c
Qinwen Xu <qinwen@google.com>
2024-04-16 22:37:01 +0000

    eval scripts

--- xpk/v5p-32_eval.sh
+++ xpk/v5p-32_eval.sh
@@ -55,14 +55,20 @@ ethtool -K "${dev_name}" tx-nocache-copy on
 
 echo "rto_setup.sh finished"
 
-OUT_DIR=gs://mlperf-exp/qinwen/stablediffusion/v5p-32/eval
+OUT_DIR=$1
 
 export JAX_TRACEBACK_FILTERING=off
 export LIBTPU_INIT_ARGS='--xla_tpu_enable_async_collective_fusion_fuse_all_gather=true --xla_tpu_megacore_fusion_allow_ags=false --xla_enable_async_collective_permute=true --xla_tpu_enable_ag_backward_pipelining=true --xla_tpu_enable_data_parallel_all_reduce_opt=true --xla_tpu_data_parallel_opt_different_sized_ops=true --xla_tpu_enable_async_collective_fusion=true --xla_tpu_enable_async_collective_fusion_multiple_steps=true --xla_tpu_overlap_compute_collective_tc=true --xla_enable_async_all_gather=true'
 
 
 #cd /maxdiffusion
-git clone -b  qinwen_eval  https://github.com/suexu1025/maxdiffusion.git maxdiffusion
+git clone -b  mlperf_4  https://github.com/suexu1025/maxdiffusion.git maxdiffusion
 cd maxdiffusion
+
 pip install .
-TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TF_CPP_MIN_LOG_LEVEL=0 python -m src.maxdiffusion.eval src/maxdiffusion/configs/base_2_base.yml run_name=v5p-32-eval base_output_directory=${OUT_DIR}
+mkdir generated_images
+mkdir output
+
+TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 TF_CPP_MIN_LOG_LEVEL=0 python -m src.maxdiffusion.eval src/maxdiffusion/configs/base_2_base.yml run_name=v5p-128-eval per_device_batch_size=16 base_output_directory=${OUT_DIR}
+
+#gsutil cp -r generated_images ${OUT_DIR}/output/
