bb8ed0d9de164882e3edfdf37047e9a30a4abc89
Qinwen Xu <qinwen@google.com>
2024-04-19 23:33:33 +0000

    Adjust FID dataloader path for eval at every checkpoint

--- src/maxdiffusion/eval.py
+++ src/maxdiffusion/eval.py
@@ -92,7 +92,7 @@ def eval_scores(config, images_directory=None):
     params = model.init(rng, jnp.ones((1, 256, 256, 3)))
 
     apply_fn = jax.jit(functools.partial(model.apply, train=False))
-    dataloader_images_directory="/".join(images_directory.split("/")[:-2])
+    dataloader_images_directory="/".join(images_directory.split("/")[:-1])
     mu, sigma = fid_score.compute_statistics_with_mmap(dataloader_images_directory, "/tmp/temp.dat", params, apply_fn, batch_size, (299, 299))
     os.makedirs(config.stat_output_directory, exist_ok=True)
     np.savez(os.path.join(config.stat_output_directory, 'stats'), mu=mu, sigma=sigma)
