9dac73e4640ec88912fa62421509769bc083f9d9
Qinwen Xu <qinwen@google.com>
2024-04-29 17:34:59 +0000

    change input type back to f32

--- src/maxdiffusion/models/vae_flax.py
+++ src/maxdiffusion/models/vae_flax.py
@@ -910,7 +910,7 @@ class FlaxAutoencoderKL(nn.Module, FlaxModelMixin, ConfigMixin):
     def init_weights(self, rng: jax.Array, eval_only: bool = False) -> FrozenDict:
         # init input tensors
         sample_shape = (1, self.in_channels, self.sample_size, self.sample_size)
-        sample = jnp.zeros(sample_shape, dtype=jnp.bfloat16)
+        sample = jnp.zeros(sample_shape, dtype=jnp.float32)
 
         params_rng, dropout_rng, gaussian_rng = jax.random.split(rng, 3)
         rngs = {"params": params_rng, "dropout": dropout_rng, "gaussian": gaussian_rng}
