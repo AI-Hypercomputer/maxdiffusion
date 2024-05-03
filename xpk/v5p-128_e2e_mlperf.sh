e877134f60424002a8e3f7130cb707214dd02718
Qinwen Xu <qinwen@google.com>
2024-05-03 17:15:59 +0000

    change workdir

--- xpk/v5p-128_e2e_mlperf.sh
+++ xpk/v5p-128_e2e_mlperf.sh
@@ -61,7 +61,7 @@ export TPU_STDERR_LOG_LEVEL=0
 export TPU_MIN_LOG_LEVEL=0
 export TF_CPP_MIN_LOG_LEVEL=0
 
-git clone -b mlperf_4 https://github.com/google/maxdiffusion maxdiffusion
+git clone -b eval_last_n_ckpt https://github.com/google/maxdiffusion maxdiffusion
 cd maxdiffusion
 
 pip install .
@@ -74,15 +74,15 @@ PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-16}
 NUM_CHECKPOINTS=${NUM_CHECKPOINTS:-10}
 NUM_DEVICES=${NUM_DEVICES:-64}
 MAX_TRAIN_STEPS=${MAX_TRAIN_STEPS:-$(( $CHECKPOINT_EVERY * $NUM_CHECKPOINTS / $PER_DEVICE_BATCH_SIZE / $NUM_DEVICES ))}
-
+LR=${LR:-1.75e-4}
 # training
 RUN_NAME=${RUN_NAME:-"mlperf_e2e"}
-OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-gs://mlperf-exp/$USER/sd}
+OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-gs://mlperf-exp/$USER/sd/$RUN_NAME}
 python -m src.maxdiffusion.models.train src/maxdiffusion/configs/base_2_base.yml run_name=$RUN_NAME base_output_directory=$OUTPUT_DIRECTORY train_data_dir=gs://jfacevedo-maxdiffusion-v5p/laion400m/processed/laion400m_moments-tfrec \
-per_device_batch_size=16 split_head_dim=True  attention=flash  norm_num_groups=16 \
+per_device_batch_size=${PER_DEVICE_BATCH_SIZE} split_head_dim=True  attention=flash  norm_num_groups=16 \
 eval_at_checkpoint=False \
 train_new_unet=True \
-warmup_steps_fraction=0.1 learning_rate=1.75e-4 \
+warmup_steps_fraction=0.1 learning_rate=${LR} \
 noise_offset=-1.0 input_peturbation=-1.0 prediction_type='v_prediction' snr_gamma=-1.0 \
 upload_images=False \
 checkpoint_every=${CHECKPOINT_EVERY} max_train_steps=$MAX_TRAIN_STEPS 2>&1 | tee /tmp/log
@@ -122,7 +122,6 @@ base_output_directory=$OUTPUT_DIRECTORY 2>&1 | tee -a /tmp/log
 done
 
 if [[ $(grep "MLLOG" /tmp/log | wc -l) -gt 0 ]];then
-  # TODO: remove --target-fid=500 --target-clip=0 once solving convergence issue
-  python src/maxdiffusion/report_end.py --metrics-path=${OUTPUT_DIRECTORY}/eval_metrics.csv --mllog-path=/tmp/log --target-fid=500 --target-clip=0 2>&1 | tee -a /tmp/log
+  python src/maxdiffusion/report_end.py --metrics-path=${OUTPUT_DIRECTORY}/eval_metrics.csv --mllog-path=/tmp/log --target-fid=90 --target-clip=0.15 2>&1 | tee -a /tmp/log
   gsutil cp /tmp/log ${OUTPUT_DIRECTORY}/log_${MEGASCALE_SLICE_ID}_${TPU_WORKER_ID}
 fi
