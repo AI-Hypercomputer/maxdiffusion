bacad3f5f7dc38f8aa1c13d625cd4ec76b0f19bf
Qinwen <qinwen@google.com>
2024-05-03 13:42:23 -0700

    only save last n ckpt (#42)
    * only save last n ckpt, NUM_CHECKPOINT=0 for every checkpoint

--- xpk/offline_eval.sh
+++ xpk/offline_eval.sh
@@ -2,10 +2,13 @@
 EVAL_OUT_DIR=/tmp/outputs
 mkdir -p $EVAL_OUT_DIR
 
-
-OUTPUT_DIRECTORY=$1
-RUN_NAME=$2
 ckpt_dir=gs://mlperf-exp/qinwen/stablediffusion/v5p-256/e2e/${RUN_NAME}
+OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-gs://mlperf-exp/$USER/sd-eval/$RUN_NAME}
+
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-16}
+NUM_CHECKPOINTS=${NUM_CHECKPOINTS:-10}
+NUM_DEVICES=${NUM_DEVICES:-64}
+MAX_TRAIN_STEPS=${MAX_TRAIN_STEPS:-$(( $CHECKPOINT_EVERY * $NUM_CHECKPOINTS / $PER_DEVICE_BATCH_SIZE / $NUM_DEVICES ))}
 
 git clone -b mlperf_4 https://github.com/google/maxdiffusion maxdiffusion
 cd maxdiffusion
@@ -13,10 +16,21 @@ cd maxdiffusion
 pip install .
 pip install git+https://github.com/mlperf/logging.git
 
+eval_sample_end=$(($MAX_TRAIN_STEPS*$PER_DEVICE_BATCH_SIZE * $NUM_DEVICES))
+echo "MLPerf Eval samples ends @" $eval_sample_end
+eval_freq=512000
+eval_sample_start=$(($eval_sample_end-$(($(($NUM_CHECKPOINTS-1))*$eval_freq))))
+
 for checkpoint_dir in $(gsutil ls $ckpt_dir/checkpoints/); do
-  checkpoint_name=$(basename $checkpoint_dir)
-  mkdir -p $EVAL_OUT_DIR/$checkpoint_name
-  python -m src.maxdiffusion.eval src/maxdiffusion/configs/base_2_base.yml run_name=$RUN_NAME per_device_batch_size=16 \
+  steptime=(${checkpoint_dir//samples_count=/ })
+  steptime=${steptime[1]}
+  steptime=(${steptime//// })
+  steptime=${steptime[0]}
+  if [ "$steptime" -ge "$eval_sample_start" ] && [ "$steptime" -le "$eval_sample_end" ]; then
+    echo "MLPerf Eval Checkpoint at "${steptime}
+    checkpoint_name=$(basename $checkpoint_dir)
+    mkdir -p $EVAL_OUT_DIR/$checkpoint_name
+    python -m src.maxdiffusion.eval src/maxdiffusion/configs/base_2_base.yml run_name=$RUN_NAME per_device_batch_size=16 \
 pretrained_model_name_or_path="${checkpoint_dir}" \
 caption_coco_file="gs://mlperf-exp/sd-copy/cocodata/val2014_30k_padded.tsv" \
 images_directory="$EVAL_OUT_DIR/$checkpoint_name" \
@@ -25,8 +39,9 @@ stat_output_file="output/stats.npz" \
 stat_coco_file="gs://mlperf-exp/sd-copy/cocodata/val2014_30k_stats.npz" \
 clip_cache_dir="clip_cache_dir" \
 base_output_directory=$OUTPUT_DIRECTORY 2>&1 | tee -a /tmp/log
-  sleep 30
-  rm -r $EVAL_OUT_DIR/$checkpoint_name
+    sleep 30
+    rm -r $EVAL_OUT_DIR/$checkpoint_name
+  fi
 done
 
 if [[ $(grep "MLLOG" /tmp/log | wc -l) -gt 0 ]];then
