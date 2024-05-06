026c7297506dd68e89cff9baa756d01ab93fbc27
Qinwen Xu <qinwen@google.com>
2024-05-06 17:40:51 +0000

    clean up

--- src/maxdiffusion/eval.py
+++ src/maxdiffusion/eval.py
@@ -134,7 +134,6 @@ def eval_scores(config, images_directory=None, checkpoint_name=None):
     sigma1 = jax.experimental.multihost_utils.process_allgather(sigma1)
     mu1 = sum(mu1)/len(mu1)
     sigma1 = sum(sigma1)/len(sigma1)
-    print(sigma1.shape)
     fid = fid_score.compute_frechet_distance(mu1, mu2, sigma1, sigma2, eps=1e-6)
     mllog_utils.eval_fid(config, fid, checkpoint_name)
     write_eval_metrics(config, clip_score, fid, checkpoint_name)
