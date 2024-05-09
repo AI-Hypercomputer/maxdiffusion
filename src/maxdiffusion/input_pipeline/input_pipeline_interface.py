503a4de141fbe873505636cbcc4f25f4a86132c2
Qinwen Xu <qinwen@google.com>
2024-05-09 22:37:23 +0000

    paralism = 8

--- src/maxdiffusion/input_pipeline/input_pipeline_interface.py
+++ src/maxdiffusion/input_pipeline/input_pipeline_interface.py
@@ -93,15 +93,13 @@ def make_laion400m_train_iterator(
 
   train_ds = (
     tf.data.Dataset.list_files(os.path.join(config.train_data_dir,"*"), shuffle=False, seed=config.seed)
-      .shard(num_shards = jax.process_count(), index = jax.process_index())
+     .interleave(tf.data.TFRecordDataset, num_parallel_calls=paralism)
       .map(_parse_tfrecord_fn, num_parallel_calls=paralism)
       .map(prepare_sample, num_parallel_calls=paralism)
       .map(lambda x, y: tf.py_function(partial_tokenize, inp=[x, y], Tout=(tf.float32, tf.float32)), num_parallel_calls=paralism)
       .map(create_dict, num_parallel_calls=paralism)
-      .shuffle(global_batch_size * 10 // jax.process_count(), seed=config.seed)
-      .batch(global_batch_size // jax.process_count(), drop_remainder=True)
-      .repeat(-1)
-      .prefetch(AUTOTUNE)
+      .shuffle(global_batch_size * 10, seed=config.seed)
+      .batch(global_batch_size // jax.process_count(), drop_remainder=False)
   )
 
   train_iter = multihost_dataloading.get_batch_sharded_data_pipeline(train_ds, mesh)
