9816800e70cff54c4a649cf201b26b89469fd828
Juan Acevedo <jfacevedo@google.com>
2024-05-20 22:33:11 +0000

    fix unit tests. Update deprecated functions.

--- src/maxdiffusion/pipelines/stable_diffusion/safety_checker_flax.py
+++ src/maxdiffusion/pipelines/stable_diffusion/safety_checker_flax.py
@@ -23,8 +23,8 @@ from transformers.models.clip.modeling_flax_clip import FlaxCLIPVisionModule
 
 
 def jax_cosine_distance(emb_1, emb_2, eps=1e-12):
-    norm_emb_1 = jnp.divide(emb_1.T, jnp.clip(jnp.linalg.norm(emb_1, axis=1), a_min=eps)).T
-    norm_emb_2 = jnp.divide(emb_2.T, jnp.clip(jnp.linalg.norm(emb_2, axis=1), a_min=eps)).T
+    norm_emb_1 = jnp.divide(emb_1.T, jnp.clip(jnp.linalg.norm(emb_1, axis=1), min=eps)).T
+    norm_emb_2 = jnp.divide(emb_2.T, jnp.clip(jnp.linalg.norm(emb_2, axis=1), min=eps)).T
     return jnp.matmul(norm_emb_1, norm_emb_2.T)
 
 
