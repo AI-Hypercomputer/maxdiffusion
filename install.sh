c65792870455e7a0783e8dfc17e18e5a4963dd98
Qinwen Xu <qinwen@google.com>
2024-10-04 17:04:55 +0000

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ git clone -b qinwen/zero https://github.com/google/maxdiffusion.git
 pip install -U --pre jax[tpu] --no-cache-dir  -f https://storage.googleapis.com/jax-releases/jax_nightly_releases.html -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install libtpu-nightly==0.1.dev20241001   -f https://storage.googleapis.com/libtpu-releases/index.html
 pip install git+https://github.com/google/jax
-pip install requirements.txt
+pip install -r requirements.txt
 sudo chmod 777 /usr/local/lib/python3.10/dist-packages/
 sudo chmod 777 /usr/local/bin/
