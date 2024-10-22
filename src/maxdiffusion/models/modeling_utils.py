cf669c473f3c464f35068c01123d9c39cf96a9f7
Juan Acevedo <jfacevedo@google.com>
2024-10-22 22:05:04 +0000

    update generate code to support hypersdxl lora.

--- src/maxdiffusion/models/modeling_utils.py
+++ src/maxdiffusion/models/modeling_utils.py
@@ -99,12 +99,6 @@ def get_parameter_dtype(parameter: torch.nn.Module):
     first_tuple = next(gen)
     return first_tuple[1].dtype
 
-# def load_lora_state_dict(cls, pretrained_model_name_or_path: Union[str, os.PathLike], **kwargs):
-#   """
-#   Load LoRA 
-#   """
-
-
 def load_state_dict(checkpoint_file: Union[str, os.PathLike], variant: Optional[str] = None):
   """
   Reads a checkpoint file, returning properly formatted errors if they arise.
