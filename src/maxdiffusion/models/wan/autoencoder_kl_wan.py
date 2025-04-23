cc11bb155f92e54cbb52bb7b39814c2fd7614953
Juan Acevedo <jfacevedo@google.com>
2025-04-23 20:11:40 +0000

    added tests for a couple of wan vae layers.

--- src/maxdiffusion/models/wan/autoencoder_kl_wan.py
+++ src/maxdiffusion/models/wan/autoencoder_kl_wan.py
@@ -22,8 +22,11 @@ from flax import nnx
 from ...configuration_utils import ConfigMixin, flax_register_to_config
 from ..modeling_flax_utils import FlaxModelMixin
 from ... import common_types
+from ..vae_flax import FlaxAutoencoderKLOutput, FlaxDiagonalGaussianDistribution
 
 BlockSizes = common_types.BlockSizes
+
+CACHE_T = 2
 
 _ACTIVATIONS = {
   "swish": jax.nn.silu,  
@@ -128,43 +131,350 @@ class WanRMS_norm(nnx.Module):
   def __init__(
     self,
     dim: int,
-    *,
+    rngs: nnx.Rngs,
+    channel_first: bool = True,
+    images: bool = True,
     eps: float = 1e-6,
     use_bias: bool = False,
-    rngs: nnx.Rngs
   ):
+    broadcastable_dims = (1, 1, 1) if not images else (1, 1)
+    shape = (dim, *broadcastable_dims) if channel_first else (dim, )
     self.eps = eps
-    shape = (dim,)
+    self.channel_first = channel_first
     self.scale = dim ** 0.5
     # Initialize gamma as parameter
-    self.gamma = nnx.Param(jax.random.ones(rngs.params(), shape))
+    self.gamma = nnx.Param(jnp.ones(shape))
     if use_bias:
-      self.bias = nnx.Param(jax.random.zeros(rngs.params(), shape))
+      self.bias = nnx.Param(jnp.zeros(shape))
     else:
-      self.bias = None
+      self.bias = 0
   
   def __call__(self, x: jax.Array) -> jax.Array:
-    # Expects input channels in the last dimension
-    variance = jnp.mean(jnp.square(x), axis=-1, keepdims=True)
-    inv_std = jax.lax.rsqrt(variance + self.eps)
-    normalized = x * inv_std * self.gamma.value * self.scale
+    normalized = jnp.linalg.norm(x, ord=2, axis=(1 if self.channel_first else -1), keepdims=True)
+    normalized = x / jnp.maximum(normalized, self.eps)
+    normalized = normalized * self.scale * self.gamma
     if self.bias:
       return normalized + self.bias.value
     return normalized
 
+class WanUpsample(nnx.Module):
+  def __init__(self, scale_factor: Tuple[float, float], method: str = 'nearest'):
+    # scale_factor for (H, W)
+    # JAX resize works on spatial dims, H, W assumming (N, D, H, W, C) or (N, H, W, C)
+    self.scale_factor = scale_factor
+    self.method = method
+  
+  def __call__(self, x: jax.Array) -> jax.Array:
+    input_dtype = x.dtype
+    in_shape = x.shape
+    is_3d = len(in_shape) == 5
+    n, d, h, w, c = in_shape if is_3d else(in_shape[0], 1, in_shape[1], in_shape[2], in_shape[3])
+
+    target_h = int(h * self.scale_factor[0])
+    target_w = int(w * self.scale_factor[1])
+
+    # jax.image.resize expects (..., H, W, C)
+    if is_3d:
+      x_reshaped = x.reshape(n * d, h, w, c)
+      out_reshaped = jax.image.resize(x_reshaped.astype(jnp.float32),
+                                      (n * d, target_h, target_w, c),
+                                      method=self.method)
+      out = out_reshaped.reshape(n, d, target_h, target_w, c)
+    else: # Asumming (N, H, W, C)
+      out = jax.image.resize(x.astype(jnp.float32),
+                             (n, target_h, target_w, c),
+                             method=self.method)
+    
+    return out.astype(input_dtype)
+
+class Identity(nnx.Module):
+  def __call__(self, x):
+    return x
+
+class ZeroPaddedConv2D(nnx.Module):
+  def __init__(
+    self,
+    dim: int,
+    rngs: nnx.Rngs,
+    flash_min_seq_length: int = 4096,
+    flash_block_sizes: BlockSizes = None,
+    mesh: jax.sharding.Mesh = None,
+    dtype: jnp.dtype = jnp.float32,
+    weights_dtype: jnp.dtype = jnp.float32,
+    precision: jax.lax.Precision = None,
+    attention: str = "dot_product",
+  ):
+    self.conv = nnx.Conv(
+      dim,
+      dim,
+      kernel_size=(1, 3, 3),
+      padding='SAME',
+      use_bias=True,
+      rngs=rngs
+    )
+  
+  def __call__(self, x):
+    # This pad assumes (B, C, H, W)
+    x = jax.lax.pad(x, 0.0, [(0, 0, 0), (0, 0, 0), (0, 1, 0), (0, 1, 0)])
+    return self.conv(x)
+
+
+class WanResample(nnx.Module):
+  def __init__(
+    self,
+    dim: int,
+    mode: str,
+    rngs: nnx.Rngs,
+    flash_min_seq_length: int = 4096,
+    flash_block_sizes: BlockSizes = None,
+    mesh: jax.sharding.Mesh = None,
+    dtype: jnp.dtype = jnp.float32,
+    weights_dtype: jnp.dtype = jnp.float32,
+    precision: jax.lax.Precision = None,
+    attention: str = "dot_product",
+  ):
+    self.dim = dim
+    self.mode = mode
+    self.time_conv = None
+
+    if mode == "upsample2d":
+      self.resample = nnx.Sequential(
+        WanUpsample(scale_factor=(2.0, 2.0), method="nearest"),
+        nnx.Conv(
+          dim,
+          dim // 2,
+          kernel_size=(1, 3, 3),
+          padding='SAME',
+          use_bias=True,
+          rngs=rngs,
+        )
+      )
+    elif mode == "upsample3d":
+      self.resample = nnx.Sequential(
+        WanUpsample(scale_factor=(2.0, 2.0), method="nearest"),
+        nnx.Conv(
+          dim,
+          dim // 2,
+          kernel_size=(1, 3, 3),
+          padding='SAME',
+          use_bias=True,
+          rngs=rngs,
+        )
+      )
+      self.time_conv = WanCausalConv3d(dim, dim * 2, (3, 1, 1), padding=(1, 0, 0), rngs=rngs)
+    elif mode == "downsample2d":
+      self.resample = ZeroPaddedConv2D(dim=dim, rngs=rngs)
+    elif mode == "downsample3d":
+      self.resample = ZeroPaddedConv2D(dim=dim, rngs=rngs)
+    else:
+      self.resample = Identity()
+
+  def __call__(self, x: jax.Array, feat_cache=None, feat_idx=[0]) -> jax.Array:
+    return x
+  
+class WanResidualBlock(nnx.Module):
+  def __init__(
+      self,
+      in_dim: int,
+      out_dim: int,
+      rngs: nnx.Rngs,
+      dropout: float = 0.0,
+      non_linearity: str = "silu",
+  ):
+    pass
+
+  def __call__(self, x: jax.Array, feat_cache=None, feat_idx=[0]):
+    return x
+
+class WanAttentionBlock(nnx.Module):
+  def __init__(
+      self,
+      dim: int,
+      rngs: nnx.Rngs
+  ):
+    self.dim = dim
+  
+  def __call__(self, x: jax.Array):
+    return x
+
+class WanMidBlock(nnx.Module):
+  def __init__(
+    self,
+    dim: int,
+    rngs: nnx.Rngs,
+    dropout: float = 0.0,
+    non_linearity: str = "silu",
+    num_layers: int = 1
+  ):
+    self.dim = dim
+  
+  def __call__(self, x: jax.Array, feat_cache=None, feat_idx=[0]):
+    return x
+
+class WanUpBlock(nnx.Module):
+  def __init__(
+    self,
+    in_dim: int,
+    out_dim: int,
+    num_res_blocks: int,
+    rngs: nnx.Rngs,
+    dropout: float = 0.0,
+    upsample_mode: Optional[str] = None,
+    non_linearity: str = "silu"
+  ):
+    # Create layers list
+    self.resnets = []
+    # Add residual blocks and attention if needed
+    current_dim = in_dim
+    for _ in range(num_res_blocks + 1):
+      self.resnets.append(WanResidualBlock(in_dim=current_dim, out_dim=out_dim, dropout=dropout, non_linearity=non_linearity, rngs=rngs))
+      current_dim = out_dim
+    
+    # Add upsampling layer if needed.
+    self.upsamplers = None
+    if upsample_mode is not None:
+      self.upsamplers = WanResample(dim=out_dim, mode=upsample_mode, rngs=rngs)
+  
+  def __call__(self, x: jax.Array, feat_cache=None, feat_idx=[0]):
+    return x
 
 class WanEncoder3d(nnx.Module):
-  pass
+  def __init__(
+    self,
+    rngs: nnx.Rngs,
+    dim: int =128,
+    z_dim: int = 4,
+    dim_mult = [1, 2, 4, 4],
+    num_res_blocks = 2,
+    attn_scales = [],
+    temperal_downsample = [True, True, False],
+    dropout = 0.0,
+    non_linearity: str = 'silu',
+  ):
+    self.dim = dim
+    self.z_dim = z_dim
+    self.dim_mult = dim_mult
+    self.num_res_blocks = num_res_blocks
+    self.attn_scales = attn_scales
+    self.temperal_downsample = temperal_downsample
+    self.nonlinearity = get_activation(non_linearity)
 
-class WanCausalConv3d(nnx.Module):
-  pass
+    # dimensions
+    dims = [dim * u for u in [1] + dim_mult]
+    scale = 1.0
+
+    # init block
+    self.conv_in = WanCausalConv3d(3, dims[0], 3, padding=1, rngs=rngs)
+
+    # downsample blocks
+    self.down_blocks = []
+    for i, (in_dim, out_dim) in enumerate(zip(dims[:-1], dims[1:])):
+      # residual (+attention) blocks
+      for _ in range(num_res_blocks):
+        self.down_blocks.append(WanResidualBlock(in_dim=in_dim, out_dim=out_dim, dropout=dropout, rngs=rngs))
+        if scale in attn_scales:
+          self.down_blocks.append(WanAttentionBlock(dim=out_dim, rngs=rngs))
+        in_dim = out_dim
+    
+      # downsample block
+      if i != len(dim_mult) - 1:
+        mode = "downsample3d" if temperal_downsample[i] else "downsample2d"
+        self.down_blocks.append(WanResample(out_dim, mode=mode, rngs=rngs))
+      
+    # middle_blocks
+    self.mid_block = WanMidBlock(out_dim, dropout, non_linearity, num_layers=1, rngs=rngs)
+
+    # output blocks
+    self.norm_out = WanRMS_norm(out_dim, images=False, rngs=rngs)
+    self.conv_out = WanCausalConv3d(in_channels=out_dim, out_channels=z_dim, kernel_size=3, padding=1)
+  
+  def __call__(self, x: jax.Array, feat_cache=None, feat_idx=[0]):
+    return x
 
 class WanDecoder3d(nnx.Module):
-  pass
+  r"""
+  A 3D decoder module.
+  Args:
+    dim (int): The base number of channels in the first layer.
+    z_dim (int): The dimensionality of the latent space.
+    dim_mult (list of int): Multipliers for the number of channels in each block.
+    num_res_blocks (int): Number of residual blocks in each block.
+    attn_scales (list of float): Scales at which to apply attention mechanisms.
+    temperal_upsample (list of bool): Whether to upsample temporally in each block.
+    dropout (float): Dropout rate for the dropout layers.
+    non_linearity (str): Type of non-linearity to use.
+  """
+  def __init__(
+    self,
+    rngs: nnx.Rngs,
+    dim: int = 128,
+    z_dim: int = 128,
+    dim_mult: List[int] = [1, 2, 4, 4],
+    num_res_blocks: int = 2,
+    attn_scales = List[float],
+    temperal_upsample=[False, True, True],
+    dropout=0.0,
+    non_linearity: str = "silu",
+  ):
+    self.dim = dim
+    self.z_dim = z_dim
+    self.dim_mult = dim_mult
+    self.num_res_blocks = num_res_blocks
+    self.attn_scales = attn_scales
+    self.temperal_upsample = temperal_upsample
+
+    self.nonlinearity = get_activation(non_linearity)
+
+    # dimensions
+    dims = [dim * u for u in [dim_mult[-1]] + dim_mult[::-1]]
+    scale = 1.0 / 2 ** (len(dim_mult) - 2)
+
+    # init block
+    self.conv_in = WanCausalConv3d(in_channels=z_dim, out_channels=dims[0], kernel_size=3, padding=1, rngs=rngs)
+
+    # middle_blocks
+    self.mid_block = WanMidBlock(dim=dims[0], rngs=rngs, dropout=dropout, non_linearity=non_linearity, num_layers=1)
+
+    # upsample blocks
+    self.up_blocks = []
+    for i, (in_dim, out_dim) in enumerate(zip(dims[:-1], dims[1:])):
+      # residual (+attention) blocks
+      if i > 0:
+        in_dim = in_dim // 2
+      
+      # Determine if we need upsampling
+      upsample_mode = None
+      if i != len(dim_mult) - 1:
+        upsample_mode = "upsample3d" if temperal_upsample[i] else "upsample2d"
+      
+      # Crete and add the upsampling block
+      up_block = WanUpBlock(
+        in_dim=in_dim,
+        out_dim=out_dim,
+        num_res_blocks=num_res_blocks,
+        dropout=dropout,
+        upsample_mode=upsample_mode,
+        non_linearity=non_linearity,
+        rngs=rngs
+      )
+      self.up_blocks.append(up_block)
+
+      # Update scale for next iteration
+      if upsample_mode is not None:
+        scale *=2.0
+    
+    # output blocks
+    self.norm_out = nnx.RMSNorm(num_features=out_dim, )
+    self.norm_out = WanRMS_norm(dim=out_dim, images=False, rngs=rngs)
+    self.conv_out = WanCausalConv3d(in_channels=out_dim, out_channels=3, kernel_size=3, padding=1, rngs=rngs)
+  
+  def __call__(self, x: jax.Array, feat_cache=None, feat_idx=[0]):
+    return x
 
 class AutoencoderKLWan(nnx.Module, FlaxModelMixin, ConfigMixin):
   def __init__(
     self,
+    rngs: nnx.Rngs,
     base_dim: int = 96,
     z_dim: int = 16,
     dim_mult: Tuple[int] = [1,2,4,4],
@@ -186,8 +496,35 @@ class AutoencoderKLWan(nnx.Module, FlaxModelMixin, ConfigMixin):
     self.temporal_upsample = temporal_downsample[::-1]
 
     self.encoder = WanEncoder3d(z_dim * 2, z_dim * 2, 1)
-    self.post_quant_conv = WanCausalConv3d(z_dim, z_dim, 1)
+    self.post_quant_conv = WanCausalConv3d(z_dim, z_dim, 1, rngs=rngs)
 
     self.decoder = WanDecoder3d(
       base_dim, z_dim, dim_mult, num_res_blocks, attn_scales, self.temporal_upsample, dropout
     )
+    self.clear_cache()
+  
+  def clear_cache(self):
+    """ Resets cache dictionaries and indices"""
+    def _count_conv3d(module):
+      count = 0
+      node_types = nnx.graph.iter_graph(module, nnx.Module)
+      for node in node_types:
+        if isinstance(node.value, WanCausalConv3d):
+          count +=1
+      return count
+
+    self._conv_num = _count_conv3d(self.decoder)
+    self._conv_idx = [0]
+    self._feat_map = [None] * self._conv_num
+    # cache encode
+    self._enc_conv_num = _count_conv3d(self.encoder)
+    self._enc_conv_idx = [0]
+    self._enc_feat_map = [None] * self._enc_conv_num
+
+
+  def encode(self, x: jax.Array, return_dict: bool = True) -> Union[FlaxAutoencoderKLOutput, Tuple[FlaxDiagonalGaussianDistribution]]:
+    """ Encode video into latent distribution."""
+    if x.shape[-1] != 3:
+      raise ValueError(f"Expected input shape (N, D, H, W, 3), got {x.shape}")
+    
+    self.clear_cache()
--- src/maxdiffusion/tests/wan_vae_test.py
+++ src/maxdiffusion/tests/wan_vae_test.py
@@ -15,16 +15,146 @@
  """
 
 import os
+import jax
+import jax.numpy as jnp
+from flax import nnx
+import numpy as np
 import unittest
 import pytest
 from absl.testing import absltest
+from ..models.wan.autoencoder_kl_wan import WanCausalConv3d, WanUpsample, AutoencoderKLWan, WanRMS_norm
 
 class WanVaeTest(unittest.TestCase):
   def setUp(self):
     WanVaeTest.dummy_data = {}
   
-  # def test_3d_conv(self):
+  # def test_clear_cache(self):
+  #   key = jax.random.key(0)
+  #   rngs = nnx.Rngs(key)
+  #   wan_vae = AutoencoderKLWan(rngs=rngs)
+  #   wan_vae.clear_cache()
 
-  
+  def test_wanrms_norm(self):
+    """Test against the Pytorch implementation"""
+    import torch
+    import torch.nn as nn
+    import torch.nn.functional as F
+
+    class TorchWanRMS_norm(nn.Module):
+      r"""
+      A custom RMS normalization layer.
+
+      Args:
+          dim (int): The number of dimensions to normalize over.
+          channel_first (bool, optional): Whether the input tensor has channels as the first dimension.
+              Default is True.
+          images (bool, optional): Whether the input represents image data. Default is True.
+          bias (bool, optional): Whether to include a learnable bias term. Default is False.
+      """
+
+      def __init__(self, dim: int, channel_first: bool = True, images: bool = True, bias: bool = False) -> None:
+        super().__init__()
+        broadcastable_dims = (1, 1, 1) if not images else (1, 1)
+        shape = (dim, *broadcastable_dims) if channel_first else (dim,)
+
+        self.channel_first = channel_first
+        self.scale = dim**0.5
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.bias = nn.Parameter(torch.zeros(shape)) if bias else 0.0
+
+      def forward(self, x):
+        return F.normalize(x, dim=(1 if self.channel_first else -1)) * self.scale * self.gamma + self.bias
+    
+    # --- Test Case 1: images == True ---
+    model = TorchWanRMS_norm(2)
+    input_shape = (1, 2, 2, 2, 3)
+    input = torch.ones(input_shape)
+    torch_output = model(input)
+    torch_output_np = torch_output.detach().numpy()
+    
+    key = jax.random.key(0)
+    rngs = nnx.Rngs(key)
+    wanrms_norm = WanRMS_norm(dim=2, rngs=rngs)
+    input_shape = (1, 2, 2, 2, 3)
+    dummy_input = jnp.ones(input_shape)
+    output = wanrms_norm(dummy_input)
+    output_np = np.array(output)
+    assert np.allclose(output_np, torch_output_np) == True
+
+    # --- Test Case 2: images == False ---
+    model = TorchWanRMS_norm(2, images=False)
+    input_shape = (1, 2, 2, 2, 3)
+    input = torch.ones(input_shape)
+    torch_output = model(input)
+    torch_output_np = torch_output.detach().numpy()
+    
+    key = jax.random.key(0)
+    rngs = nnx.Rngs(key)
+    wanrms_norm = WanRMS_norm(dim=2, rngs=rngs, images=False)
+    input_shape = (1, 2, 2, 2, 3)
+    dummy_input = jnp.ones(input_shape)
+    output = wanrms_norm(dummy_input)
+    output_np = np.array(output)
+    assert np.allclose(output_np, torch_output_np) == True
+
+  def test_wan_upsample(self):
+    batch_size=1
+    in_depth, in_height, in_width = 10, 32, 32
+    in_channels = 3
+
+    dummy_input = jnp.ones((batch_size, in_depth, in_height, in_width, in_channels))
+
+    upsample = WanUpsample(scale_factor=(2.0, 2.0))
+
+    # --- Test Case 1: depth > 1 ---
+    output = upsample(dummy_input)
+    assert output.shape == (1, 10, 64, 64, 3)
+
+    in_depth = 1
+    dummy_input = jnp.ones((batch_size, in_depth, in_height, in_width, in_channels))
+    # --- Test Case 1: depth == 1 ---
+    output = upsample(dummy_input)
+    assert output.shape == (1, 1, 64, 64, 3)
+
+  def test_3d_conv(self):
+    key = jax.random.key(0)
+    rngs = nnx.Rngs(key)
+    batch_size=1
+    in_depth, in_height, in_width = 10, 32, 32
+    in_channels = 3
+    out_channels = 16
+    kernel_d, kernel_h, kernel_w = 3, 3, 3 # Kernel size (Depth, Height, Width)
+    padding_d, padding_h, padding_w = 1, 1, 1 # Base padding (Depth, Height, Width)
+
+    # Create dummy input data
+    dummy_input = jnp.ones((batch_size, in_depth, in_height, in_width, in_channels))
+
+    # Create dummy cache data (from a previous step)
+    cache_depth = 2 * padding_d
+    dummy_cache = jnp.zeros((batch_size, cache_depth, in_height, in_width, in_channels))
+
+    # Instantiate the module
+    causal_conv_layer = WanCausalConv3d(
+      in_channels=in_channels,
+      out_channels=out_channels,
+      kernel_size=(kernel_d, kernel_h, kernel_w),
+      padding=(padding_d, padding_h, padding_w),
+      rngs=rngs # Pass rngs for initialization
+    )
+
+    # --- Test Case 1: No Cache ---
+    output_no_cache = causal_conv_layer(dummy_input)
+    assert output_no_cache.shape == (1, 10, 32, 32, 16)
+
+    # --- Test Case 2: With Cache ---
+    output_with_cache = causal_conv_layer(dummy_input, cache_x=dummy_cache)
+    assert output_with_cache.shape == (1, 10, 32, 32, 16)
+
+    # --- Test Case 3: With Cache larger than padding ---
+    larger_cache_depth = 4 # Larger than needed padding (2*padding_d = 2)
+    dummy_larger_cache = jnp.zeros((batch_size, larger_cache_depth, in_height, in_width, in_channels))
+    output_with_larger_cache = causal_conv_layer(dummy_input, cache_x=dummy_larger_cache)
+    assert output_with_larger_cache.shape == (1, 10, 32, 32, 16)
+
 if __name__ == "__main__":
   absltest.main()
