e38675baf2a897532b10a80593c610a707598edf
Haixin Liu <haixinl@nvidia.com>
2025-10-01 20:16:59 -0700

    fix for gpu

--- src/maxdiffusion/train_flux.py
+++ src/maxdiffusion/train_flux.py
@@ -19,6 +19,7 @@ from typing import Sequence
 import jax
 from absl import app
 from maxdiffusion import (max_logging, pyconfig)
+from contextlib import contextmanager
 
 from maxdiffusion.train_utils import (
     validate_train_config,
@@ -39,6 +40,24 @@ def main(argv: Sequence[str]) -> None:
   max_logging.log(f"Found {jax.device_count()} devices.")
   train(config)
 
+@contextmanager
+def transformer_engine_context():
+  """ If TransformerEngine is available, this context manager will provide the library with MaxText-specific details needed for correcct operation. """
+  try:
+    from transformer_engine.jax.sharding import global_shard_guard, MeshResource
+    # Inform TransformerEngine of MaxText's physical mesh resources.
+    mesh_resource = MeshResource(
+      dp_resource = "data",
+      tp_resource = "tensor",
+      fsdp_resource = "fsdp",
+      pp_resource = None,
+      cp_resource = None,
+    )
+    with global_shard_guard(mesh_resource):
+      yield
+  except ImportError:
+    yield
 
 if __name__ == "__main__":
-  app.run(main)
+  with transformer_engine_context():
+    app.run(main)
