a83bb1c85883b9ecded2a1e258d2a64a38033de2
susanbao <sanbao_google_com@t1v-n-216c02cd-w-0.europe-west4-b.c.cloud-tpu-multipod-dev.internal>
2025-12-11 23:57:25 +0000

    qwix

--- src/maxdiffusion/pipelines/wan/wan_pipeline.py
+++ src/maxdiffusion/pipelines/wan/wan_pipeline.py
@@ -302,8 +302,8 @@ class WanPipeline:
             act_qtype=jnp.float8_e4m3fn,
             bwd_qtype=jnp.float8_e5m2,
             disable_channelwise_axes=True,  # per_tensor calibration
-            weight_calibration_method=config.quantization_calibration_method,
-            act_calibration_method=config.quantization_calibration_method,
+            weight_calibration_method="fixed,-224,224",
+            act_calibration_method="fixed,-224,224",
             bwd_calibration_method=config.quantization_calibration_method,
             op_names=("dot_general", "einsum"),
         ),
@@ -313,8 +313,8 @@ class WanPipeline:
             act_qtype=jnp.float8_e4m3fn,
             bwd_qtype=jnp.float8_e4m3fn,
             disable_channelwise_axes=True,  # per_tensor calibration
-            weight_calibration_method=config.quantization_calibration_method,
-            act_calibration_method=config.quantization_calibration_method,
+            weight_calibration_method="fixed,-224,224",
+            act_calibration_method="fixed,-224,224",
             bwd_calibration_method=config.quantization_calibration_method,
             op_names=("conv_general_dilated"),
         ),
