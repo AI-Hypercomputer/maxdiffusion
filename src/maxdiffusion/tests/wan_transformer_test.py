22a87b8914cc3d9b783de88a28caadaffa19c391
susanbao <susanbaonju@gmail.com>
2025-12-12 14:48:41 +0000

    fix unit test

--- src/maxdiffusion/tests/wan_transformer_test.py
+++ src/maxdiffusion/tests/wan_transformer_test.py
@@ -343,8 +343,8 @@ class WanTransformerTest(unittest.TestCase):
             act_qtype=jnp.float8_e4m3fn,
             bwd_qtype=jnp.float8_e5m2,
             disable_channelwise_axes=True,  # per_tensor calibration
-            weight_calibration_method=config_fp8_full.quantization_calibration_method,
-            act_calibration_method=config_fp8_full.quantization_calibration_method,
+            weight_calibration_method="fixed,-224,224",
+            act_calibration_method="fixed,-224,224",
             bwd_calibration_method=config_fp8_full.quantization_calibration_method,
             op_names=("dot_general", "einsum"),
         ),
@@ -354,8 +354,8 @@ class WanTransformerTest(unittest.TestCase):
             act_qtype=jnp.float8_e4m3fn,
             bwd_qtype=jnp.float8_e4m3fn,
             disable_channelwise_axes=True,  # per_tensor calibration
-            weight_calibration_method=config_fp8_full.quantization_calibration_method,
-            act_calibration_method=config_fp8_full.quantization_calibration_method,
+            weight_calibration_method="fixed,-224,224",
+            act_calibration_method="fixed,-224,224",
             bwd_calibration_method=config_fp8_full.quantization_calibration_method,
             op_names=("conv_general_dilated"),
         ),
