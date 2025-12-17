2f0450e8e5994ca2370d0e88168e186acec4cda3
Prisha Jain <prishajain@google.com>
2025-12-17 23:52:33 +0530

    wan_utils.py fixed

--- src/maxdiffusion/models/wan/wan_utils.py
+++ src/maxdiffusion/models/wan/wan_utils.py
@@ -256,8 +256,10 @@ def load_base_wan_transformer(
     for pt_key, tensor in tensors.items():
       renamed_pt_key = rename_key(pt_key)
       if "image_embedder" in renamed_pt_key:
-        renamed_pt_key = renamed_pt_key.replace("net.0", "net_0")
-        renamed_pt_key = renamed_pt_key.replace("net.2", "net_2")
+        if "net.0" in renamed_pt_key:
+          renamed_pt_key = renamed_pt_key.replace("net.0", "net_0.proj")
+        elif "net.2" in renamed_pt_key:
+          renamed_pt_key = renamed_pt_key.replace("net.2", "net_2.proj")
         renamed_pt_key = renamed_pt_key.replace("norm1", "norm1.layer_norm")
         if "norm1" in renamed_pt_key or "norm2" in renamed_pt_key:
           renamed_pt_key = renamed_pt_key.replace("kernel", "scale")
