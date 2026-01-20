99853d2ba33b974e24505dd85e0c7caf80537ebb
Juan Acevedo <juancevedo@gmail.com>
2026-01-20 23:36:08 +0000

    update configs for unit tests.

--- src/maxdiffusion/tests/wan_transformer_test.py
+++ src/maxdiffusion/tests/wan_transformer_test.py
@@ -125,7 +125,7 @@ class WanTransformerTest(unittest.TestCase):
 
       encoder_hidden_states_shape = (batch_size, time_freq_dim * 2, text_embed_dim)
       dummy_encoder_hidden_states = jnp.ones(encoder_hidden_states_shape)
-      temb, timestep_proj, encoder_hidden_states, encoder_hidden_states_image = layer(
+      temb, timestep_proj, encoder_hidden_states, _, _ = layer(
           dummy_timestep, dummy_encoder_hidden_states
       )
       assert temb.shape == (batch_size, dim)
