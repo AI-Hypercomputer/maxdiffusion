f9b6ff9058704bc22ac905985e93f066aa0e771b
prishajain1 <prishajain@google.com>
2026-01-22 00:17:37 +0530

    Shard video_condition to prevent OOM in WAN 2.2 I2V  (#313)
    * sharding before call to vae encoder
    
    * sharding before call to vae encoder
    
    * ruff check
    
    * pyink checks
    
    * pyink check

--- src/maxdiffusion/models/ltx_video/transformers_pytorch/attention.py
+++ src/maxdiffusion/models/ltx_video/transformers_pytorch/attention.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 import inspect
 from importlib import import_module
 from typing import Any, Dict, Optional, Tuple
