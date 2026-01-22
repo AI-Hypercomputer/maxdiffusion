f50f6024d6a5d348638a1b6dac91c04cdfb64c57
Prisha Jain <prishajain@google.com>
2026-01-22 23:55:37 +0530

    Removed unused var

--- src/maxdiffusion/pipelines/wan/wan_pipeline.py
+++ src/maxdiffusion/pipelines/wan/wan_pipeline.py
@@ -544,7 +544,6 @@ class WanPipeline:
     vae_dtype = getattr(self.vae, "dtype", jnp.float32)
     video_condition = video_condition.astype(vae_dtype)
     with self.mesh, nn_partitioning.axis_rules(self.config.logical_axis_rules):
-      data_axis_name = self.config.mesh_axes[0]
       data_mesh_size = self.mesh.shape[self.config.mesh_axes[0]]
       if video_condition.shape[0] % data_mesh_size == 0:
         sharding_spec = P(self.config.mesh_axes[0], None, None, None, None)
