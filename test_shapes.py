2e78f8eb5eaf4445994af47880e99dbdcd04d4ac
Prisha Jain <prishajain@google.com>
2026-03-04 10:13:49 +0530

    adding test_shapes

---
+++ test_shapes.py
@@ -0,0 +1,8 @@
+from safetensors.torch import load_file
+from huggingface_hub import hf_hub_download
+
+safetensor_path = hf_hub_download("Lightricks/LTX-2", "audio_vae/model.safetensors")
+state_dict = load_file(safetensor_path)
+
+print("latents_mean shape:", state_dict["latents_mean"].shape)
+print("latents_std shape:", state_dict["latents_std"].shape)
