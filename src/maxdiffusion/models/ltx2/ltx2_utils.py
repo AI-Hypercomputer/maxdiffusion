4244f4c9c04e9c2399c9a8e323a53dd2c2f1412f
Prisha Jain <prishajain@google.com>
2026-03-04 15:17:36 +0530

    fix in ltx2_utils.py

--- src/maxdiffusion/models/ltx2/ltx2_utils.py
+++ src/maxdiffusion/models/ltx2/ltx2_utils.py
@@ -389,7 +389,7 @@ def load_vocoder_weights(
       
       if flax_key[-1] == "kernel":
            if "upsamplers" in flax_key:
-               tensor = tensor.transpose(2, 0, 1)
+               tensor = tensor.transpose(2, 0, 1)[::-1, :, :]
            else:
                tensor = tensor.transpose(2, 1, 0)
                
