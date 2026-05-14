cc6140158c7b0fdded3c530443dad8b5b7a6937d
Prisha Jain <prishajain@google.com>
2026-05-14 12:34:49 +0530

    Tag and save Pallas LSE statistics to eliminate attention recomputation entirely

--- src/maxdiffusion/models/flux/transformers/transformer_flux_flax.py
+++ src/maxdiffusion/models/flux/transformers/transformer_flux_flax.py
@@ -495,7 +495,7 @@ class FluxTransformer2DModel(nn.Module, FlaxModelMixin, ConfigMixin):
     # 4. Force strict checkpointing on the Single Wrapper
     #RemattedSingleWrapper = nn.remat(ScannedSingleBlockWrapper, prevent_cse=True, policy=cp.checkpoint_dots_with_no_batch_dims)
     #RemattedSingleWrapper = nn.remat(ScannedSingleBlockWrapper, prevent_cse=True, policy=cp.offload_dot_with_no_batch_dims(offload_src="device", offload_dst="pinned_host"))
-    RemattedSingleWrapper = nn.remat(ScannedSingleBlockWrapper, prevent_cse=True, policy=cp.save_only_these_names("attn_output", "lin1_norm_hidden_states"))
+    RemattedSingleWrapper = nn.remat(ScannedSingleBlockWrapper, prevent_cse=True, policy=cp.save_only_these_names("attn_output", "lin1_norm_hidden_states", "pallas_logsumexp"))
 
     self.scanned_single_blocks = nn.scan(
         RemattedSingleWrapper,
